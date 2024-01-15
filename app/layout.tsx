a56fab5e0df0d34987f5f2942b14e032200b949a
i2xv <ramadanesng@gmail.com>
2024-01-15 10:01:41 +0000

    add asstes and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,39 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+  variable: "--font-inter",
+});
+
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering questions. Get help, share knowledge, and collaborate with dvelopers from around the world",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +41,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink:
+            " primary-text-gradient hover:text-primary-50",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body
+          className={`${inter.variable} ${SpaceGrotesk.variable}`}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
